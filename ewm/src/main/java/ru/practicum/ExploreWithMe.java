a8f74f3bf4d856875079f1b1d7ea49261ed84e1a
Svetlana <tchmykova@yandex.ru>
2023-11-13 16:07:52 +0500

    try with 2 modules

---
+++ ewm/src/main/java/ru/practicum/ExploreWithMe.java
@@ -0,0 +1,9 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+
+public class ExploreWithMe {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMe.class, args);
+    }
+}
